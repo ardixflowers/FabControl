503bc2db2d0c41a156e6215ee7d4fd8a8db1653d
Lucho <ayalaluciano877@gmail.com>
2024-09-09 00:44:01 -0300

    Add files via upload
    ahora si

---
+++ fabcontrol.sql
@@ -0,0 +1,112 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Base de datos: `fabcontrol`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `muebles`
+--
+
+CREATE TABLE `muebles` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `tipo` varchar(50) NOT NULL,
+  `material` varchar(50) NOT NULL,
+  `color` varchar(30) NOT NULL,
+  `dimensiones` varchar(50) NOT NULL,
+  `peso` varchar(50) NOT NULL,
+  `precio` decimal(10,2) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `disponibles` int(11) NOT NULL,
+  `encargados` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `encargos`
+--
+
+CREATE TABLE `encargos` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_mueble` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `date_out` varchar(255) NOT NULL,
+  `date_return` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `apellido_paterno` varchar(30) NOT NULL,
+  `apellido_materno` varchar(30) NOT NULL,
+  `domicilio` varchar(250) DEFAULT NULL,
+  `tel` varchar(25) DEFAULT NULL,
+  `sanctions` int(11) DEFAULT '0',
+  `sanc_money` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+INSERT INTO muebles VALUES(1,"Pampa","Silla","Madera","Roble","40x40x80cm","5kg","250000","48","37","7");
+INSERT INTO usuarios VALUES(1,"Luciano","Ayala","Cernetti","Picoloro 6443","+54 9 11 3333-4444","","");
+INSERT INTO usuarios VALUES(2,"Guillermina","Acevedo","x","La Chacra 4234","+54 9 11 2323-2564","","");
+
+
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `muebles`
+--
+ALTER TABLE `muebles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `encargos`
+--
+ALTER TABLE `encargos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `muebles`
+--
+ALTER TABLE `muebles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `encargos`
+--
+ALTER TABLE `encargos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
---
+++ src/main/java/com/mycompany/modelos/Muebles.java
@@ -0,0 +1,103 @@
+package com.mycompany.modelos;
+
+public class Muebles {
+    private int id;
+    private String nombre;
+    private String tipo;
+    private String material;
+    private String color;
+    private String dimensiones;
+    private String peso;
+    private String precio;
+    private int stock;
+    private int disponibles;
+    private int encargados;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setDimenciones(String dimensiones) {
+        this.dimensiones = dimensiones;
+    }
+
+    public void setPeso(String peso) {
+        this.peso = peso;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public void setDisponibles(int disponibles) {
+        this.disponibles = disponibles;
+    }
+    
+    public void setEncargados(int encargados) {
+        this.encargados = encargados;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getDimenciones() {
+        return dimensiones;
+    }
+
+    public String getPeso() {
+        return peso;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+    
+    public int getStock() {
+        return stock;
+    }
+
+    public int getDisponibles() {
+        return disponibles;
+    }
+    
+    public int getEncargados() {
+        return encargados;
+    }
+}
